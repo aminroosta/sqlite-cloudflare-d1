0269a61ec557be7461c8e098d4583c942024ae33
amin <amin.roosta@outlook.com>
2023-01-01 18:52:45 -0500

    Implement createMany

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export type Row = Record<string, string | number | null | ArrayBuffer>;
+export type Value = string | number | null | ArrayBuffer;
 
 export async function create({
   db,
@@ -16,10 +17,41 @@ export async function create({
   const values_ = values.map((_) => "?").join(",");
 
   const query = `INSERT INTO ${table} (${keys_}) VALUES (${values_}) RETURNING *;`;
-  console.log({ query });
 
   return await db
     .prepare(query)
     .bind(...values)
     .first<Row>();
+}
+
+export async function createMany({
+  db,
+  table,
+  data,
+}: {
+  db: D1Database;
+  table: string;
+  data: Row[];
+}) {
+  const keys = Object.keys(data[0]);
+  const keys_ = keys.join(",");
+
+  const qs_ = "(" + keys.map((_) => "?").join(",") + ")";
+  const values_ = data.map((_) => qs_).join(",");
+
+  const values: Value[] = [];
+  data.forEach((row) => values.push(...keys.map((key) => row[key])));
+
+  const query = `INSERT INTO ${table} (${keys_}) VALUES ${values_} RETURNING *`;
+
+  const { results, success, error } = await db
+    .prepare(query)
+    .bind(...values)
+    .all();
+
+  if (!success) {
+    throw new Error(error);
+  }
+
+  return results as Row[];
 }
--- src/test.ts
+++ src/test.ts
@@ -3,9 +3,25 @@ import * as lib from "./index";
 function randstr() {
   return (Math.random() + 1).toString(36).substring(2);
 }
+function remove_ids(object: any) {
+  const remove = (object: any) => {
+    const copy = { ...object };
+    Object.keys(object).forEach((key) => {
+      if (key.endsWith("Id")) {
+        delete copy[key];
+      }
+    });
+    return copy;
+  };
+
+  if (Array.isArray(object)) {
+    return object.map((obj) => remove(obj));
+  }
+  return remove(object);
+}
 function assert(left: any, right: any) {
-  let left_ = JSON.stringify(left, null, 2);
-  let right_ = JSON.stringify(right, null, 2);
+  let left_ = JSON.stringify(remove_ids(left), null, 2);
+  let right_ = JSON.stringify(remove_ids(right), null, 2);
   if (left_ != right_) {
     throw new Error(`${left_} != ${right_}`);
   }
@@ -28,7 +44,22 @@ async function test_create(db: D1Database) {
     data: { Name: name },
   });
 
-  assert(result, { ArtistId: result.ArtistId, Name: name });
+  assert(result, { Name: name });
+}
+
+async function test_create_many(db: D1Database) {
+  const data = [
+    { Name: randstr(), Age: 31 },
+    { Name: randstr(), Age: 32 },
+  ];
+
+  const result = await lib.createMany({
+    db: db,
+    table: "men",
+    data,
+  });
+
+  assert(result, data);
 }
 
 export default {
@@ -38,8 +69,9 @@ export default {
     ctx: ExecutionContext
   ): Promise<Response> {
     try {
-      await test_select_star(env.db);
-      await test_create(env.db);
+      // await test_select_star(env.db);
+      // await test_create(env.db);
+      // await test_create_many(env.db);
     } catch (error: any) {
       return new Response(`${error.message}\n`, { status: 200 });
     }
